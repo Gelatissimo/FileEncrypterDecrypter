2e2603222045f33349a3bee238ae069b214877df
JasonCarloObiso <jasoncarlo.obiso@cit.edu>
2025-02-08 17:48:38 +0800

    Separated Encrypt and Decrypt functions

--- FileEncrypterDecrypter/Form1.cs
+++ FileEncrypterDecrypter/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,8 @@ namespace FileEncrypterDecrypter
 {
     public partial class FileEncrypterDecrypter_ObisoJ : Form
     {
+        private readonly byte[] key = Encoding.UTF8.GetBytes("aB1cD2eF3gH4iJ5kL6mN7oP8qR9sT0uV");
+
         public FileEncrypterDecrypter_ObisoJ()
         {
             InitializeComponent();
@@ -30,38 +33,65 @@ namespace FileEncrypterDecrypter
 
         private void openFileDialog_Encrypt_FileOk(object sender, CancelEventArgs e)
         {
-            FileStream fs = new FileStream(openFileDialog_Encrypt.FileName, FileMode.OpenOrCreate);
-
-            int[] temp = new int[fs.Length];
-
-            for (int x = 0; x < fs.Length; x++) { temp[x] = fs.ReadByte(); }
-            for (int x = 0; x < fs.Length; x++)
-            {
-                temp[x] = temp[x] + 3; //to be changed
-            }
-            fs.Close();
-
-            fs = new FileStream(openFileDialog_Encrypt.FileName, FileMode.OpenOrCreate);
-            for (int x = 0; x < fs.Length; x++) { fs.WriteByte((byte)temp[x]); }
-            fs.Close();
+            EncryptFile(openFileDialog_Encrypt.FileName);
         }
 
         private void openFileDialog_Decrypt_FileOk(object sender, CancelEventArgs e)
         {
-            FileStream fs = new FileStream(openFileDialog_Decrypt.FileName, FileMode.OpenOrCreate);
-
-            int[] temp = new int[fs.Length];
-
-            for (int x = 0; x < fs.Length; x++) { temp[x] = fs.ReadByte(); }
-            for (int x = 0; x < fs.Length; x++)
-            {
-                temp[x] = temp[x] - 3; //to be changed
-            }
-            fs.Close();
-
-            fs = new FileStream(openFileDialog_Decrypt.FileName, FileMode.OpenOrCreate);
-            for (int x = 0; x < fs.Length; x++) { fs.WriteByte((byte)temp[x]); }
-            fs.Close();
+            DecryptFile(openFileDialog_Decrypt.FileName);
         }
+
+        private void EncryptFile(string filePath)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.GenerateIV();
+
+                string tempFilePath = filePath + ".enc";
+                using (FileStream fs = new FileStream(tempFilePath, FileMode.Create))
+                {
+                    fs.Write(aes.IV, 0, aes.IV.Length);
+                    using (CryptoStream cryptoStream = new CryptoStream(fs, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    using (FileStream inputFs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                    {
+                        inputFs.CopyTo(cryptoStream);
+                    }
+                }
+            }
+
+            File.Delete(filePath); // Ensure the original file is closed before deleting
+            File.Move(filePath + ".enc", filePath);
+
+            MessageBox.Show("Encryption successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void DecryptFile(string filePath)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key;
+                string tempFilePath = filePath + ".dec";
+
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+                {
+                    byte[] iv = new byte[aes.BlockSize / 8];
+                    fs.Read(iv, 0, iv.Length);
+                    aes.IV = iv;
+
+                    using (FileStream outputFs = new FileStream(tempFilePath, FileMode.Create))
+                    using (CryptoStream cryptoStream = new CryptoStream(fs, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        cryptoStream.CopyTo(outputFs);
+                    }
+                }
+            }
+
+            File.Delete(filePath); // Ensure the original file is closed before deleting
+            File.Move(filePath + ".dec", filePath);
+
+            MessageBox.Show("Decryption successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
